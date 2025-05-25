29fbd2ff5be8ae5d11a2f980d385fbd5714384e9
tarikozturk017 <tarik.ozturkk1@gmail.com>
2025-05-25 18:29:28 -0400

    fixing tiny baseurl issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE + "/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
